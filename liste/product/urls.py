218eb3fb57ba50c40d818a9dd320303868134c19
umut <umt9735@gmail.com>
2019-07-23 14:46:47 +0300

    product detail view created

--- liste/product/urls.py
+++ liste/product/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path,include
-from .views import ProductCreate
+from .views import ProductCreate,ProductDetail
 
 app_name = "product"
 
 
 urlpatterns = [
-    path('create/',ProductCreate.as_view(),name="product_create")
+    path('create/',ProductCreate.as_view(),name="product_create"),
+    path('detail/<int:pk>',ProductDetail.as_view(),name="product_detail")
 
 ]
